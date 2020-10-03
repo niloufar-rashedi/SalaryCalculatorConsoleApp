61ea7b23e1943ea89f828605cb0dc6b11544803b
NiloufarRashedi <niloufar.rashedi@gmail.com>
2020-10-03 20:19:39 +0200

    Three most expensive salaries to be found

--- SalaryCalculator.cs
+++ SalaryCalculator.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using RandomNameGeneratorLibrary;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -15,37 +16,63 @@ namespace SalaryCalculator
         private static int[] months = new int[20];
 
         //Number of months that an employee has worked is this company is unique for each employee
-        Random rn = new Random();
+        private static Random rn = new Random();
 
         public void Run()
         {
-            ShowAllEmployeeInformation();
+            CreateListOfEmployee();
+            //ShowAllEmployeeInformation();
         }
 
-        private decimal CalculateSalaryForEachEmployee( int empID)
+        private static decimal CalculateSalaryForEachEmployee(int empID)
         {
             int h = hours[empID];
             int t = months[empID];
             double salary = 0;
             //Defining the condition for base salary of a full time employee
-            if(h == 169)
-                {
-                    salary = MonthlySalary;
-                }
+            if (h == 169)
+            {
+                salary = MonthlySalary;
+            }
             if (salary > 3 * MonthlySalary)
-                {           
-                     return 3 * MonthlySalary;
-                }
+            {
+                return 3 * MonthlySalary;
+            }
 
             //Defining the condition for base salary of an employee-per-hour
             else
-                {
-                    salary = h * HourlyWage;
-                    salary = (int)(salary * Math.Pow(1.05, t / 5)); 
-                }
+            {
+                salary = h * HourlyWage;
+                salary = (int)(salary * Math.Pow(1.05, t / 5));
+            }
             return (decimal)salary;
         }
-        private decimal CalculateTotalSalary()
+        private static void CreateListOfEmployee()
+        {
+            for (int i = 0; i < Employees; i++)
+            {
+                var personGenerator = new PersonNameGenerator();
+                for (int j = 0; j < Employees; j++)
+                {
+                    var fictionalEmployee = personGenerator.GenerateRandomFirstAndLastName();
+                    //Console.WriteLine(fictionalEmployee);
+                }
+                //Defining paid-per-hour employees and full-time employee
+                int workedHours = rn.Next(100);
+                months[i] = workedHours;
+                if (i > 13)
+                {
+                    workedHours = rn.Next(8, 169);
+                    hours[i] = workedHours;
+                }
+                else
+                {
+                    hours[i] = 169;
+                }
+            }
+
+        }
+        private static decimal CalculateTotalSalary()
         {
             decimal sum = 0;
             for (int i = 0; i < Employees; i++)
@@ -64,8 +91,13 @@ namespace SalaryCalculator
                     CalculateSalaryForEachEmployee(i));
             }
             Console.WriteLine(sb.ToString());
-            Console.WriteLine($"Total summa av lönerna för den månaden: {DateTime.Now.Month} " , CalculateTotalSalary());
+            Console.WriteLine($"Total summa av lönerna för den månaden: {DateTime.Now.Month} ", CalculateTotalSalary());
         }
 
+        //static int[] FindThreeHighestSalaries()
+        //{
+
+        //}
+        //private static void PrintThreeMostExpensiveEmployees()
     }
 }
